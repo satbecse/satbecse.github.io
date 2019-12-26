a884e4af3cb221de82ca27dfb0cdfcf7cb550696
satbecse <35561386+satbecse@users.noreply.github.com>
2019-12-26 14:19:52 -0600

    removed overlapFactor value Duplicate

--- specGen/lib/properties.js
+++ specGen/lib/properties.js
@@ -3,6 +3,5 @@ const confidenceThreshold=0.99                    // confidenceThreshold has be 
 const overlapFactorThreshold=0.75                 // overlapFactorThreshold has be between 0 to 1
 const fftSizeValue=2048                           // fftSizeValue has to be a power of 2
 const sampleRateHzValue=44100
-const overlapFactorValue=0.75                     // It has between 0 to 1
 const numFramesPerSpectrogramValue=43             // 
 const columnTruncateLengthValue=232               //
