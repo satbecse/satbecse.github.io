f1926f306e543e2dc3b2de9d285e91a83aa9363d
Sathishkumar Thangavel <satbecse@gmail.com>
2020-01-08 16:44:39 -0600

    updated the inputUrl in the properties.js

--- specGenMic/lib/properties.js
+++ specGenMic/lib/properties.js
@@ -1,4 +1,4 @@
-const inputUrl="https://satbecse.github.io/specGen/"
+const inputUrl="https://satbecse.github.io/specGenMic/"
 const confidenceThreshold=0.99                    // confidenceThreshold has be between 0 to 1
 const overlapFactorThreshold=0.75                 // overlapFactorThreshold has be between 0 to 1
 const fftSizeValue=2048                           // fftSizeValue has to be a power of 2
