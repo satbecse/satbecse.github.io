11e4b7e8ae9caf3f93fa4c2c61667c1e5737f494
Sathishkumar Thangavel <satbecse@gmail.com>
2020-01-10 01:44:11 -0600

    Changed the Input URL and removed tf.min.js.map

--- specGenFinal/lib/properties.js
+++ specGenFinal/lib/properties.js
@@ -1,4 +1,5 @@
-const inputUrl="http://127.0.0.1:8080/"
+//const inputUrl="http://127.0.0.1:8080/"
+const inputUrl="https://satbecse.github.io/specGenFinal"
 const confidenceThreshold=0.99                    // confidenceThreshold has be between 0 to 1
 const overlapFactorThreshold=0.75                 // overlapFactorThreshold has be between 0 to 1
 const fftSizeValue=2048                           // fftSizeValue has to be a power of 2
